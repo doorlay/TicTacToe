9b1310b3df257f57c0f04201ee002c47a49693e2
doorlay <nic@doorlay.com>
2019-10-22 15:06:21 -0700

    added files

---
+++ testFunctions.py
@@ -0,0 +1,19 @@
+# Project 3 - Tic-Tac-Toe Simulator
+# 
+# Name: Your Group Members Name
+# Instructor: S. Einakian 
+# Section: Your Section
+# Functions definitions comes here
+
+import unittest
+from tictactoeFuncs import *
+
+class TestCases(unittest.TestCase):
+   def test_func1(self):
+
+      
+
+# Run the unit tests.
+if __name__ == '__main__':
+   unittest.main()
+
---
+++ tictactoeFuncs.py
@@ -0,0 +1,38 @@
+# Project 3 - Tic-Tac-Toe Simulator
+# 
+# Name: Your Group Members Name
+# Instructor: S. Einakian 
+# Section: Your Section
+# Functions definitions comes here
+
+# def Welcome Function
+   #your codes goes here
+   
+ 
+#def createBoard Function
+   #your codes goes here
+   
+#def printBoard Function
+   #your codes goes here
+    
+#def pickLetter Function
+   #your codes goes here
+
+#def getInput Function
+   #your codes goes here
+   
+#def checkRows Function
+   #your codes goes here
+   
+#def checkCols Function
+   #your codes goes here
+   
+#def checkDiags Function
+   #your codes goes here
+  
+   
+#def checkWin Function
+   #your codes goes here
+   
+#def turnCount Function
+   #your codes goes here
