f0e5513948bfc215cf59a2ef21dfb6e2301ec94a
Richard Rios <rrios07@unix3.csc.calpoly.edu>
2019-10-26 18:06:48 -0700

    I changed a couple functions

--- tictactoeFuncs.py
+++ tictactoeFuncs.py
@@ -5,16 +5,22 @@
 # Section: 6
 # Functions definitions comes here
 
-# def Welcome Function
-   #your codes goes here
+def Welcome(): 
+    print("Welcome to Tic-Tac-Toe.\nYour goal is to get 3 in a row.\nyou will either pick X or O. X will go first.")
+    players = int(input("Do you wish to play against a (1)computer, or with (2)Players? "))
+    return players
    
  
-#def createBoard Function
-   #your codes goes here
+def createBoard():
+    board = []
+    for i in range (9):
+        board.append(i)
+    return board
+  
    
-#def printBoard Function
-   #your codes goes here
-    
+def printBoard(list):
+   
+
 #def pickLetter Function
    #your codes goes here
 
@@ -37,4 +43,4 @@
 #def turnCount Function
    #your codes goes here
 
-)
+
